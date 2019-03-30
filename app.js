706ab70a3b243cde266df5789314fc0c8bf18c43
CodeXRook <lacekitfly@gmail.com>
2019-03-30 18:46:05 -0400

    create app use

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 //ROUTES
-
 const userRoutes = require('./routes/user')
 
+
 const app = express();
+
+const port =3000;
+
+app.use(cors());
